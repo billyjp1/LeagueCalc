533d2a56150cbec5c06b013473ff84a48564989f
billyjp1 <74315433+billyjp1@users.noreply.github.com>
2021-08-21 01:25:16 -0400

    Added except for when something goes wrong reading, currently only happens with Nidalee

--- StatScraper.py
+++ StatScraper.py
@@ -7,28 +7,31 @@ champions_file.close()
 
 
 def getStats(champion):
-    URL = "https://leagueoflegends.fandom.com/wiki/{}/LoL".format(champion.replace("\n",""))
-    # Debug
-    print("Going to url {}".format(URL))
+    # Go to champs wiki page
+    URL = "https://leagueoflegends.fandom.com/wiki/{}/LoL".format(champion)
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, "html.parser")
 
+    # Find all their ability dmg info
     results = soup.find(id="mw-content-text")
-
     abilities = results.find_all("div", class_="skill_leveling")
+    
+    print("Current champ: {}".format(champion))
 
-    fileName = "ScrapedData\{}Test.txt".format(champion.replace("\n",""))
+    fileName = "ScrapedData\{}Test.txt".format(champion)
     file = open(fileName, "w", encoding="utf-8")
 
     for ability in abilities:
         damage_scaling = ability.find("dd")
-        file.write(damage_scaling.text.replace("<small>", ".").replace("</small>", ""))
+        try: 
+            file.write(damage_scaling.text.replace("<small>", ".").replace("</small>", ""))
+        except:
+            print("Went wrong at {}".format(champion))
         file.write("\n")
 
     file.close()
 
 for champ in champions:
-    getStats(champ.replace(" ", "_"))
+    getStats(champ.replace(" ", "_").replace("\n",""))
 
 
