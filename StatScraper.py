a14d9cf43d3a5cc723708f168fa1b1f0c2258b9a
billyjp1 <74315433+billyjp1@users.noreply.github.com>
2021-08-21 02:20:53 -0400

    Getting all attributes for active abilities, passives not working yet

--- StatScraper.py
+++ StatScraper.py
@@ -12,23 +12,39 @@ def getStats(champion):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
 
+    abilities = []
+
     # Find all their ability dmg info
     results = soup.find(id="mw-content-text")
-    abilities = results.find_all("div", class_="skill_leveling")
+    passive_ability = results.find("div", class_="skill skill_innate")
+    abilities.append(results.find("div", class_="skill skill_q"))
+    abilities.append(results.find("div", class_="skill skill_w"))
+    abilities.append(results.find("div", class_="skill skill_e"))
+    abilities.append(results.find("div", class_="skill skill_r"))
     
     print("Current champ: {}".format(champion))
 
     fileName = "ScrapedData\{}Test.txt".format(champion)
     file = open(fileName, "w", encoding="utf-8")
 
-    for ability in abilities:
-        damage_scaling = ability.find("dd")
-        try: 
-            file.write(damage_scaling.text.replace("<small>", ".").replace("</small>", ""))
-        except:
-            print("Went wrong at {}".format(champion))
-        file.write("\n")
+    # Passive is written sepretley, different format from normal abilites
+    file.write("Passive:\n{}\n".format(passive_ability.find("span").text))
 
+    ability_char = ["Q", "W", "E", "R"]
+    for i in range(4):
+        # For each ability the champ the champ has, find all damage stats present
+        file.write("{} ability :\n".format(ability_char[i]))
+        try: 
+            # If champ has multiple attributes to their ability, get them all
+            all_stats = abilities[i].find_all("dd")
+            for stat in all_stats:
+                file.write(stat.text.replace("<small>", ".").replace("</small>", ""))
+                file.write("\n")
+        except:
+            # If only one attribute, just get it
+            single_stat = abilities[i].find("dd")
+            file.write(single_stat.text.replace("<small>", ".").replace("</small>", ""))
+        file.write("\n")     
     file.close()
 
 for champ in champions:
